9d76e7d9ea1977eb8048a9fd3c0ed122c6c09122
Talal <talalothmanse@gmail.com>
2022-12-02 17:30:51 +0800

    -Finish 'about' page

--- pages/about.js
+++ pages/about.js
@@ -13,34 +13,26 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 export default function Home() {
     const router = useRouter()
 
-    
+
     return (
         // <Layout props={0x249aaa}>
-            <div className={styles.aboutContent}>
-                <div className={styles.aboutCard}>
-                    <div className={styles.aboutImageContainer}>
-                        <div className={styles.aboutImageItem}>
-                            <Image
-                                src="/images/Talal.jpg"
-                                alt="Picture of the author"
-                                width={500}
-                                height={500}
-                                className={styles.aboutImageItem}
-                            />
-                        </div>
-                        <h1 className={styles.aboutNameItem}>Talal Al-Ahdal</h1>
+        <div className={styles.aboutContent}>
+            <div className={styles.aboutCard}>
+                <div className={styles.aboutImageContainer}>
+                    <div className={styles.aboutImageItem}>
+                        <Image
+                            src="/images/Talal.jpg"
+                            alt="Picture of the author"
+                            width={500}
+                            height={500}
+                            className={styles.aboutImageItem}
+                        />
                     </div>
+                    <h1 className={styles.aboutNameItem}>Talal Al-Ahdal</h1>
                     <div className={styles.aboutSocialMediaContainer}>
                         <Link href="https://www.linkedin.com/in/talalothman/">
                             <FontAwesomeIcon
                                 icon={faLinkedin}
-                                style={{ fontSize: 25 }}
-                                className={styles.aboutNavbarLogoItem}
-                            /></Link>
-
-                        <Link href="https://talal-portfolio.herokuapp.com">
-                            <FontAwesomeIcon
-                                icon={faGlobe}
                                 style={{ fontSize: 25 }}
                                 className={styles.aboutNavbarLogoItem}
                             /></Link>
@@ -52,19 +44,19 @@ export default function Home() {
                                 className={styles.aboutNavbarLogoItem}
                             /></Link>
                     </div>
-                    <div className={styles.aboutDonationItem}>
-                        <Link href="https://www.buymeacoffee.com/talalOthman">
-                        <Image
-                            src="/images/buymecoffe.jpeg"
-                            alt="Picture of the author"
-                            width={500}
-                            height={500}
-                            className={styles.aboutDonationItem}
-                        />
-                        </Link>
-                    </div>
+                </div>
+                <div className={styles.aboutDescriptionContainer}>
+                    <p className={styles.aboutDescriptionItem}>Extremely motivated to constantly develop my skills and grow
+                        professionally. I am confident in my ability to tackle any obstacles I might face in the future.</p>
+                </div>
+
+                <div className={styles.aboutResumeContainer}>
+                    <Link href="https://drive.google.com/file/d/1kLzgIBUgkwgYh-6ibkYAiGfhUEHQi_sE/view?usp=share_link" passHref={true}>
+                        <button className={`${styles.indexButtonItem} ${styles.projectContentButton}`}>Resume</button>
+                    </Link>
                 </div>
             </div>
+        </div>
         // </Layout>
     )
 }
