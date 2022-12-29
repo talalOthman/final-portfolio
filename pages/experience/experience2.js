8eca58f17fbcfb14926c4ab47003c6afb0534992
Talal <talalothmanse@gmail.com>
2022-12-29 19:19:47 +0800

    -Created a custom component for each experience

--- pages/experience/experience2.js
+++ pages/experience/experience2.js
@@ -1,106 +1,23 @@
-import Link from 'next/link';
-import Layout from '../components/layout';
-import styles from '../../styles/Home.module.scss'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faArrowAltCircleRight, faArrowLeft, faArrowRight, faCode, faProjectDiagram, faShieldVirus, faShip, faVirusCovid } from "@fortawesome/free-solid-svg-icons";
-import { useRouter } from 'next/router'
-import { faGithub, faLinkedin, faReact, faPython, faJs, faCss3, faCss3Alt, faHtml5, faLaravel } from '@fortawesome/free-brands-svg-icons';
-
+import React from 'react'
+import ExperienceCard from '../components/experienceCard';
 
 
 export default function Experience2() {
-    const router = useRouter()
-
+    const contributions = [
+        'Developed the entire frontend of the new stock terminal that will be used by future customers to track their stock.',
+        'Improved the customer experience compared to the old stock terminal used by the customers.',
+        'Implemented all the frontend functionalities that were requested by relevant stakeholders in a timely manner.'
+    ]
     return (
-        <div className={styles.projectsContent}>
-            <div className={styles.projectsContainer}>
-                <div className={styles.projectsNameContainer}>
-                    <h1 className={styles.projectsNameItem}>IME Technology</h1>
-                </div>
-                <div className={styles.projectsPicContainer}>
-                    <div className={styles.projectsPicItem}>
-                        <FontAwesomeIcon
-                            icon={faVirusCovid}
-                            style={{ fontSize: 250 }}
-                            className={styles.projectsPicItem}
-                        />
-                    </div>
-                </div>
-                <div className={styles.projectsDescriptionContainer}>
-                    <p className={styles.projectsDescriptionItem}>
-                        A web application that provide the latest data relevant to the covid-19 pandameic. The data that is being used throughout this web application
-                        is fetched by using several third-party APIs.
-                    </p>
-                </div>
-                <div className={styles.projectsTechContainer}>
-                    <h3 className={styles.projectsTechTitleItem}>Technologies Used</h3>
-                    <div className={styles.projectsTechIconsContainer}>
-                        <FontAwesomeIcon
-                            icon={faJs}
-                            style={{ fontSize: 25 }}
-                            className={styles.projectsTechIconItem}
-                        />
-
-                        <FontAwesomeIcon
-                            icon={faCss3Alt}
-                            style={{ fontSize: 25 }}
-                            className={styles.projectsTechIconItem}
-                        />
-
-                        <FontAwesomeIcon
-                            icon={faHtml5}
-                            style={{ fontSize: 25 }}
-                            className={styles.projectsTechIconItem}
-                        />
-                    </div>
-                </div>
-                <div className={styles.projectsBottomContentContainer}>
-
-                    <Link href="/experience/experienceMain" passHref={true}>
-                        <FontAwesomeIcon
-                            icon={faArrowLeft}
-                            style={{ fontSize: 25 }}
-                            className={`${styles.projectsTechIconItem} ${styles.projectsTechIconItemHidden}`}
-                        />
-                    </Link>
-
-                    <div className={styles.projectsLinksContainer}>
-                        <Link href="https://github.com/talalOthman/Covid19-Tracker" passHref={true}>
-                            <a className={`${styles.projectContentButton}`} target="_blank">
-                                Github
-                            </a>
-                        </Link>
-                        <Link href="https://covid-19-tracker-74390.web.app/" passHref={true}>
-                            <a className={`${styles.projectContentButton}`} target="_blank">
-                                Demo
-                            </a>
-                        </Link>
-                    </div>
-                    <FontAwesomeIcon
-                        icon={faArrowRight}
-                        style={{ fontSize: 25 }}
-                        className={`${styles.projectsTechIconItemDisabled} ${styles.projectsTechIconItemHidden}`}
-                    />
-                </div>
-            </div>
-
-            <div className={styles.projectsNavigatorContainer}>
-                <Link href="/experience/experienceMain" passHref={true}>
-                    <FontAwesomeIcon
-                        icon={faArrowLeft}
-                        style={{ fontSize: 25 }}
-                        className={styles.projectsTechIconItem}
-                    />
-                </Link>
-
-
-                <FontAwesomeIcon
-                    icon={faArrowRight}
-                    style={{ fontSize: 25 }}
-                    className={styles.projectsTechIconItemDisabled}
-                />
-
-            </div>
-        </div>
+        <ExperienceCard
+            jobTitle={'Frontend Developer'}
+            image={'/images/IME.png'}
+            startDuration={'2021 July'}
+            endDuration={'2021 Oct'}
+            contributions={contributions}
+            detailsLink={'https://imetech.com.my/'}
+            rightArrow={'Disabled'}
+            leftArrowLink={'/experience/experienceMain'}
+        />
     )
 }
--- pages/experience/experienceMain.js
+++ pages/experience/experienceMain.js
@@ -1,89 +1,23 @@
-import Link from 'next/link';
-import Layout from '../components/layout';
-import styles from '../../styles/Home.module.scss'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faArrowAltCircleRight, faArrowLeft, faArrowRight, faCode, faProjectDiagram, faShieldVirus } from "@fortawesome/free-solid-svg-icons";
-import { useRouter } from 'next/router'
-import { faGithub, faLinkedin, faReact, faPython, faJs, faCss3, faCss3Alt, faHtml5 } from '@fortawesome/free-brands-svg-icons';
-import React, { useEffect } from 'react'
-import Image from 'next/image'
+import React from 'react'
+import ExperienceCard from '../components/experienceCard';
 
 
 export default function ExperienceMain() {
-    const router = useRouter()
-
+    const contributions = [
+        'Developed 7 new notifications that are being used by customers.',
+        'Enhanced existing model classes by creating custom scope functions to ease the development for future features.',
+        'Increased the overall engagement rate of the Vimigo product.'
+    ]
     return (
-        <div className={styles.experienceContent}>
-            <div className={styles.experienceContainer}>
-                <div className={styles.experienceNameContainer}>
-                    <h1 className={styles.projectsNameItem}>Backend Developer</h1>
-                </div>
-                <div className={styles.experiencePicContainer}>
-                <div className={styles.experienceImageItem}>
-                        <Image
-                            src="/images/vimigo.png"
-                            width={500}
-                            height={500}
-                            className={styles.experienceImageItem}
-                        />
-                    </div>
-                </div>
-                <div className={styles.experienceDateContainer}>
-                    <p className={styles.experienceDateTitle}>Experience Duration</p>
-                    <p className={styles.experienceDateItem}>2021 Oct - 2022 Feb</p>
-                </div>
-                {/* <div className={styles.experienceTitleContainer}>
-                <p className={styles.experienceTitleItem}>Key Contributions</p>
-                </div> */}
-                <div className={styles.experienceDescriptionContainer}>
-                <p className={styles.experienceTitleItem}>Key Contributions</p>
-                    <ul className={styles.experienceDescriptionList}>
-                        <li className={styles.experienceDescriptionItem}>Developed 7 new notifications that are being used by customers.</li>
-                        <li className={styles.experienceDescriptionItem}>Enhanced existing model classes by creating custom scope functions to ease the development for future features</li>
-                        <li className={styles.experienceDescriptionItem}>Increased the overall engagement rate of the Vimigo product.</li>
-                    </ul>
-                </div>
-                <div className={styles.projectsBottomContentContainer}>
-
-                    <FontAwesomeIcon
-                        icon={faArrowLeft}
-                        style={{ fontSize: 25 }}
-                        className={`${styles.projectsTechIconItemDisabled} ${styles.projectsTechIconItemHidden}`}
-                    />
-                    <div className={styles.projectsLinksContainer}>
-                        <Link href="https://www.vimigotech.com/" passHref={true}>
-                            <a className={`${styles.projectContentButton}`} target="_blank">
-                                Details
-                            </a>
-                        </Link>
-                    </div>
-                    <Link href="/experience/experience2" passHref={true}>
-                        <FontAwesomeIcon
-                            icon={faArrowRight}
-                            style={{ fontSize: 25 }}
-                            className={`${styles.projectsTechIconItem} ${styles.projectsTechIconItemHidden}`}
-                        />
-                    </Link>
-                </div>
-            </div>
-
-            <div className={styles.projectsNavigatorContainer}>
-
-                <FontAwesomeIcon
-                    icon={faArrowLeft}
-                    style={{ fontSize: 25 }}
-                    className={styles.projectsTechIconItemDisabled}
-                />
-
-
-                <Link href="/experience/experience2" passHref={true}>
-                    <FontAwesomeIcon
-                        icon={faArrowRight}
-                        style={{ fontSize: 25 }}
-                        className={styles.projectsTechIconItem}
-                    />
-                </Link>
-            </div>
-        </div>
+        <ExperienceCard
+            jobTitle={'Backend Developer'}
+            image={'/images/vimigo.png'}
+            startDuration={'2021 Oct'}
+            endDuration={'2022 Feb'}
+            contributions={contributions}
+            detailsLink={'https://www.vimigotech.com/'}
+            leftArrow={'Disabled'}
+            rightArrowLink={'/experience/experience2'}
+        />
     )
 }
